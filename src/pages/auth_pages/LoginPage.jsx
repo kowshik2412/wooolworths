c0e0503851bbe5e03dbc12b1f0b44448e391c6ba
kowshik2412 <131274050+kowshik2412@users.noreply.github.com>
2023-05-28 23:12:58 +0930

    updated auth pages

--- src/pages/auth_pages/LoginPage.jsx
+++ src/pages/auth_pages/LoginPage.jsx
@@ -1,25 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../firebase.config.js';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { loginUser } from "../../apis/auth.api";
 import { loadStorage, saveStorage } from "../../utils/persistLocalStorage";
+import { useNavigate } from "react-router-dom";
 import './auth.scss';
-import { toast } from 'react-toastify';
 import Header from "../../components/header/Header";
-import { GoogleButton } from 'react-google-button';
-import {
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
-import UseAuth from "../../hooks/useAuth.js";
 
 const LoginPage = () => {
+  const user = loadStorage("user");
   const navigate = useNavigate();
-  const loation = useLocation();
-  const user = UseAuth();
-  const from = loation.state?.from || '/';
 
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
 
@@ -29,59 +19,24 @@ const LoginPage = () => {
     }
   }, []);
 
-  const handleLogin = async (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
-    if (!email || !password) {
+    if (!username || !password) {
       return;
     }
 
-    const userCredential = await signInWithEmailAndPassword(auth, email, password)
-
-    const user = userCredential.user
-    saveStorage("user", user);
-    navigate("/");
-
-    // loginUser({ username, password })
-    //   .then((res) => {
-    //     console.log(res);
-    //     saveStorage("token", res.data["tokens"]);
-    //     saveStorage("user", res.data["user"]);
-    //     navigate("/");
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
-  };
-
-  const googleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then(result => {
-        const { displayName, uid } = result.user.uid;
-        console.log(result.user);
-        saveStorage("user", user);
-        // navigate("/");
-        navigate(
-          from,
-          {
-            replace: true,
-            state: { uid, userName: displayName },
-          }
-        );
+    loginUser({ username, password })
+      .then((res) => {
+        console.log(res);
+        saveStorage("token", res.data["tokens"]);
+        saveStorage("user", res.data["user"]);
+        navigate("/");
       })
-      .catch(err => console.log(err));
-
-  }
-  const handleGoogleSignIn = async () => {
-    try {
-      googleSignIn();
-    }
-    catch (error) {
-      console.log(error);
-      toast.error(error.message);
-    }
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="auth">
@@ -89,17 +44,16 @@ const LoginPage = () => {
         <div className="auth__container__header">
           <p className="header__title">Welcome back!</p>
         </div>
-        <GoogleButton onClick={handleGoogleSignIn} />
-        {/* <form className="auth__form">
+        <form className="auth__form">
           <div className="auth__form__group">
             <label className="form__group__label">
-              Email
+              Username
             </label>
             <input
               className="form__group__input"
               type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="auth__form__group">
@@ -127,7 +81,7 @@ const LoginPage = () => {
         <div className="auth__switch">
           <span>Don't have an account?</span>
           <a href="/register">Sign Up</a>
-        </div> */}
+        </div>
       </div>
     </div>
   );
--- src/pages/auth_pages/RegistrationPage.js
+++ src/pages/auth_pages/RegistrationPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { loadStorage } from '../../utils/persistLocalStorage';
 import { useNavigate } from 'react-router-dom';
+import { registerUser } from '../../apis/auth.api';
 
 function RegistrationPage() {
 	const user = loadStorage("user");
@@ -25,14 +26,14 @@ function RegistrationPage() {
 			return;
 		}
 
-		// registerUser({ username, email, name, password })
-		// 	.then((res) => {
-		// 		console.log(res);
-		// 		navigate("/login");
-		// 	})
-		// 	.catch((err) => {
-		// 		console.log(err);
-		// 	});
+		registerUser({ username, email, name, password })
+			.then((res) => {
+				console.log(res);
+				navigate("/login");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	};
 
 	return (
