048cdc1a2164cad35ceabbce44ddf54dd6a66a9d
kowshik2412 <131274050+kowshik2412@users.noreply.github.com>
2023-05-29 23:55:31 +0930

    updated profile page to show orders

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -3,68 +3,58 @@ import './profilePage.scss';
 import Header from '../../components/header/Header';
 import { delStorage, loadStorage, saveStorage } from '../../utils/persistLocalStorage';
 import { useNavigate } from 'react-router';
+import useGetAddresses from '../../hooks/useGetAddresses';
+import { db } from '../../firebase.config';
+import { addDoc, collection } from 'firebase/firestore';
+import useGetOrders from '../../hooks/useGetOrders';
+import { formatDateTime } from '../../utils/utils';
 
 const ProfilePage = () => {
 	const user = loadStorage("user");
-	console.log(user);
 	const navigate = useNavigate();
-	const [isLoading, setIsLoading] = useState(true);
+	const [isAddressCreateModalOpen, setIsAddressCreateModalOpen] = useState(false);
 
-	const [userData, setUserData] = useState({});
+	useEffect(() => {
+		if (!user) {
+			navigate("/login");
+		}
+	}, [user]);
 
-	// useEffect(() => {
-	// 	fetchUserData();
-	// }, []);
-
-	// const fetchUserData = async () => {
-	// 	getSingleUser(user.id, accessToken)
-	// 		.then((res) => {
-	// 			console.log(res);
-	// 			setUserData(res.data.user);
-	// 			saveStorage("user", res.data.user);
-	// 			setIsLoading(false);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.log(err);
-	// 			setIsLoading(false);
-	// 		});
-	// };
-
-	// const fetchUserAddresses = async () => { };
-
+	const { addresses, loading: addressLoading, refetch: addressRefetch } = useGetAddresses(user?.uid);
+	const { orders, loading: orderLoading, refetch: orderRefetch } = useGetOrders(user?.uid);
 	// const fetchUserOrders = async () => { };
 
 	return (
 		<>
 			<Header />
-			<div class="page_container">
-				<div class="container">
-					<div class="profile-section">
-						<div class="profile-info">
-							<div class="profile-image">
-								<img src={user.photoURL} alt="Profile Image" />
+			<div className="page_container">
+				<div className="container">
+					<div className="profile-section">
+						<div className="profile-info">
+							<div className="profile-image">
+								<img src={user?.photoURL} alt="Profile Image" />
 							</div>
-							<div class="info-row">
-								<span class="info-label">Name:</span>
-								<span class="info-value">
-									{user.displayName}
+							<div className="info-row">
+								<span className="info-label">Name:</span>
+								<span className="info-value">
+									{user?.displayName}
 								</span>
 							</div>
-							<div class="info-row">
-								<span class="info-label">Email:</span>
-								<span class="info-value">
-									{user.email}
+							<div className="info-row">
+								<span className="info-label">Email:</span>
+								<span className="info-value">
+									{user?.email}
 								</span>
 							</div>
-							<div class="info-row">
-								<span class="info-label">Phone:</span>
-								<span class="info-value">
-									{user.phone || 'N/A'}
+							<div className="info-row">
+								<span className="info-label">Phone:</span>
+								<span className="info-value">
+									{user?.phone || 'N/A'}
 								</span>
 							</div>
 						</div>
-						<div class="logout-btn">
-							<button class="btn btn-primary"
+						<div className="logout-btn">
+							<button className="btn btn-primary"
 								onClick={() => {
 									delStorage("user");
 									navigate("/login");
@@ -72,39 +62,121 @@ const ProfilePage = () => {
 							>Logout</button>
 						</div>
 					</div>
-					<div class="address_list">
-						<h2 class="section-title">Addresses</h2>
-						<div class="address-list">
-							<div class="address-item">
-								<span class="address-label">Shipping Address:</span>
-								<span class="address-value">123 Main St, City, State, ZIP</span>
-							</div>
-							<div class="address-item">
-								<span class="address-label">Billing Address:</span>
-								<span class="address-value">456 Elm St, City, State, ZIP</span>
-							</div>
+					<div className="address_list">
+						<div className='title_sec'>
+							<h2 className="section-title">Addresses</h2>
+
+							<button className="add-btn btn-primary"
+								onClick={() => {
+									setIsAddressCreateModalOpen(true);
+								}}
+							>Add Address</button>
+						</div>
+						<div className="address-list">
+							{
+								addresses.map((item, index) => (
+									<div className="address-item" key={index}>
+										<span className="address-value">Address: {item.address}</span>
+										<span className="address-value">Phone: {item.phone}</span>
+									</div>
+								))
+							}
 						</div>
 					</div>
-					<div class="order-history">
-						<h2 class="section-title">Order History</h2>
-						<div class="order-list">
-							<div class="order-item">
-								<span class="order-number">Order #001</span>
-								<span class="order-date">Date: 2023-05-14</span>
-								<span class="order-total">Total: $99.99</span>
-							</div>
-							<div class="order-item">
-								<span class="order-number">Order #002</span>
-								<span class="order-date">Date: 2023-05-10</span>
-								<span class="order-total">Total: $49.99</span>
-							</div>
+					<div className="order-history">
+						<h2 className="section-title">Order History</h2>
+						<div className="order-list">
+							{
+								!orderLoading && orders.map((item, index) => (
+
+									<div className="order-item">
+										<span className="order-number">Order #{index + 1}</span>
+										<span className="order-date">Date: {formatDateTime(item.created_at)}</span>
+										<span className="order-total">Total: {item.total} $</span>
+									</div>
+								))
+							}
 						</div>
 					</div>
 				</div>
 			</div>
 
+			{
+				isAddressCreateModalOpen && <CreateAddressModal
+					user={user}
+					onClose={() => {
+						setIsAddressCreateModalOpen(false);
+						addressRefetch(true);
+					}}
+				/>
+			}
 		</>
 	);
 };
 
 export default ProfilePage;
+
+
+const CreateAddressModal = ({ user, onClose }) => {
+	const [address, setAddress] = useState("");
+	const [phone, setPhone] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState("");
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const data = {
+			address,
+			phone,
+			user_id: user.uid,
+		};
+		await addDoc(collection(db, "addresses"), data);
+		onClose();
+	};
+
+	return (
+		<div className="modal">
+			<div className="modal-content">
+				<div className="modal-header">
+					<h2 className="modal-title">Add Address</h2>
+					<span className="close"
+						onClick={() => {
+							onClose();
+						}}
+					>&times;</span>
+				</div>
+				<div className="modal-body">
+					<form onSubmit={handleSubmit}>
+						<div className="form-group">
+							<label htmlFor="address">Address</label>
+							<input
+								type="text"
+								className="form-control"
+								id="address"
+								placeholder="Enter Address"
+								value={address}
+								onChange={(e) => setAddress(e.target.value)}
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="phone">Phone</label>
+							<input
+								type="text"
+								className="form-control"
+								id="phone"
+								placeholder="Enter Phone"
+								value={phone}
+								onChange={(e) => setPhone(e.target.value)}
+							/>
+						</div>
+						<button type="submit" className="btn btn-primary">
+							{loading ? <span>Loading...</span> : <span>Add</span>}
+						</button>
+						{error && <div className="error">{error}</div>}
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+
+}
